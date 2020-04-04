650f36a81d8cdf6f7dd01807c288fbdf45b6c45d
Deniz OZOGUL <deniz.ozogul@bilgeadam.com>
2020-04-04 16:57:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const crypto = require('crypto')
+const express = require('express')
+const bodyParser = require('body-parser')
+const argv = require('yargs').argv
+const shell = require('shelljs')
+
+const secret = argv.secret;
+
+const sigHeaderName = 'X-Hub-Signature'
+const sigHeaderEventName = 'X-GitHub-Event'
+
+const app = express()
+app.use(bodyParser.json())
+
+function verifyPostData(req, res, next) {
+    const payload = JSON.stringify(req.body)
+    if (!payload) {
+        return next('Request body empty')
+    }
+    const sig = req.get(sigHeaderName) || ''
+    const hmac = crypto.createHmac('sha1', secret)
+    const digest = Buffer.from('sha1=' + hmac.update(payload).digest('hex'), 'utf8')
+    const checksum = Buffer.from(sig, 'utf8')
+    if (checksum.length !== digest.length || !crypto.timingSafeEqual(digest, checksum)) {
+        return next(`Request body digest (${digest}) did not match ${sigHeaderName} (${checksum})`)
+    }
+    return next()
+}
+
+app.post('/webhook', verifyPostData, function (req, res) {
+    const event = req.get(sigHeaderEventName) || ''
+    if (event == 'push') {  
+        shell.exec('./publish.sh')
+    } else {
+        res.status(200).send('Request body was signed')
+    }
+})
+
+app.use((err, req, res, next) => {
+    if (err) console.error(err)
+    res.status(403).send('Request body was not signed or verification failed')
+})
+
+app.listen(9000)
---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+IFS=$(printf '\n\t')
+
+# Docker
+git pull
+docker system prune -f --volumes
+sudo docker-compose down
+sudo docker build -t coreapp .
+sudo docker-compose up -d
+
+
+
+
